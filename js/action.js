2b087041b9774166772d9f88b0a68c8e63e0fba3
zhawj159753 <zhawj159753@163.com>
2013-09-29 15:24:30 +0800

    提示框完成

---
+++ js/action.js
@@ -0,0 +1,27 @@
+function showType() {
+	/*document.getElementById("image_01").src = 'img/reputation.png';
+	document.getElementById("image_01").setAttribute("style","z-index:300;position:absolute;");*/
+	
+}
+
+function showBack() {
+	/*document.getElementById("image_01").src = 'img/icon_06.png';*/
+	document.getElementById("pannel_01").setAttribute("style","display:none");
+}
+
+function zoom() {
+	var c = document.getElementById("content");
+	c.setAttribute("style","zoom:1");
+}
+
+$(".link-border ul li img").hover(function() {
+	var cur_img =$(this).parents("li");
+	cur_img.data("setTime",setTimeout(function(){
+		cur_img.addClass("hover").find(".app-info-pannel").offset({left:cur_img.offset().left+20, top:cur_img.offset().top});
+	}), 100);
+}, function() {
+	var cur_img =$(this).parents("li");
+	clearTimeout(cur_img.data("setTime"));
+	cur_img.removeClass("hover");
+}
+);
